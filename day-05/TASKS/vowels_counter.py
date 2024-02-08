9f625e02fd8b0c7c9391641b5dd6140858c5d428
Joyrel <baladjayjoyrelg20@gmail.com>
2024-02-09 02:10:14 +0800

    Update vowels_counter.py

--- day-05/TASKS/vowels_counter.py
+++ day-05/TASKS/vowels_counter.py
@@ -1,8 +1,8 @@
 # Write the code ↓ to prompt the user to enter a word.
 # Be cautious when reading input of various data types.
+print("Vowel Counter for ALF")
 
-
-
+string = input("Enter a string: ")
 
 
 
@@ -10,16 +10,17 @@
 
 # Write the code ↓ to count the number of vowels in the entered word.
 # Utilize string functions to iterate through the characters and identify vowels.
-
-
-
-
-
-
-
+def count_vowels(string):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in string:
+        if char in vowels:
+            count += 1
+    return count
 # Write the code ↓ to display the count of vowels in the word.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
-        
+num_vowels = count_vowels(string)
+print("Number of vowels in the string '{}': {}".format(string, num_vowels))        
 
 
 
