fdaa60f24eae9e19f1fdbac62f1263d0700de44e
joyyirel <baladjayjoyrelg20@gmail.com>
2024-02-09 00:36:09 +0800

    Update department_caption.py

--- day-01/TASKS/department_caption.py
+++ day-01/TASKS/department_caption.py
@@ -1,13 +1,15 @@
 # Template for department's caption
-""" 🚀Deploying...I'm <𝗡𝗮𝗺𝗲>, representing the <𝗗𝗲𝗽𝗮𝗿𝘁𝗺𝗲𝗻𝘁>. I'm thrilled to commence our expedition into the vast and uncharted realm of the <𝗚𝗮𝗹𝗮𝘅𝘆>. Together, we'll navigate this exciting frontier of technology! 🌌🌟💻 """
 
-# Write the code ↓ to read user's input.
-
+name =input("Enter your name: ")
+department =input("Enter your AWS cloud club department: ")
+galaxy =input("Specify the galaxy to which you belong: ")
 
 
 
 
 # Write the code ↓ to display the department's caption.
+print("🚀Deploying...I'm" + name + ", representing the" + department +". I'm thrilled to commence our expedition into the vast and uncharted realm of the"+ galaxy+". Together, we'll navigate this exciting frontier of technology! 🌌🌟💻")
+
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
